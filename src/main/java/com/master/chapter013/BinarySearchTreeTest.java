651b8ea6e0d4e6c0c3c6c1767b30fb9ac2dfa80c
Master <1204332316@qq.com>
2021-07-19 11:05:47 +0800

    2021/7/19 11:05AM

--- src/main/java/com/master/chapter013/BinarySearchTreeTest.java
+++ src/main/java/com/master/chapter013/BinarySearchTreeTest.java
@@ -8,7 +8,7 @@ package com.master.chapter013;
  */
 public class BinarySearchTreeTest {
     public static void main(String[] args) {
-        int[]array={7,3,10,12,5,1,9};
+        int[]array={7,3,10,12,5,1,9,2};
         BinarySortTree tree=new BinarySortTree();
         //循环添加节点到二叉排序树
         for(int i=0;i<array.length;i++){
@@ -16,8 +16,18 @@ public class BinarySearchTreeTest {
         }
         //中序遍历二叉排序树(如果对一个二叉排序树进行中序遍历，它刚好是一个有序（一般为升序）的数组)
         tree.infixOrder();
-
-
+        //测试删除叶子节点
+        //tree.deleteNode(2);
+        //System.out.println("删除叶子节点2后的遍历：");
+        //tree.infixOrder();
+        //测试删除只有一个子节点的节点
+        //tree.deleteNode(1);
+        //System.out.println("删除子节点1后的遍历：");
+        //tree.infixOrder();
+        //删除有两颗子树的节点
+        tree.deleteNode(10);
+        System.out.println("删除子节点10后的遍历：");
+        tree.infixOrder();
     }
 }
 
@@ -64,6 +74,94 @@ class BinarySortTree{
         }else {
             System.out.println("二叉排序树为空，不能遍历");
         }
+    }
+
+    //查找要删除的节点
+    public Node search(int value){
+        if(root==null){
+            return null;
+        }else{
+            return root.search(value);
+        }
+    }
+
+    //查找父节点
+    public Node searchParent(int value){
+        if(root==null){
+            return null;
+        }else{
+            return root.searchParent(value);
+        }
+    }
+    //编写方法
+    /**
+     * description: 返回以node为根节点的二叉排序树的最小节点的值.
+     * version 0.1.0
+     * @return int 以node为根节点的二叉排序树的最小节点的值
+     * @param node 传入的节点（当作二叉排序树的根节点）
+     * @author ColorXJH
+     * @date 2021/7/19 10:03
+     */
+    public int delRightTreeMin(Node node){
+        Node target=node;
+        //循环的查找左节点，就会找到最小值
+        while(target.left!=null){
+            target=target.left;
+        }
+        //这时，target就指向了最小节点
+        //删除最小节点
+        deleteNode(target.value);
+        //返回最小值
+        return target.value;
+    }
+
+    //删除节点
+    public void deleteNode(int value){
+        if(root==null){
+            return;
+        }else{
+            //1:需要先去找到要删除的节点
+            Node targetNode=search(value);
+            //如果没有找到要删除的节点，则返回
+            if(targetNode==null){return;}
+            //如果我们发现当前这颗二叉排序树只有一个节点
+            if(root.left==null&&root.right==null){
+                root=null;
+            }
+            //去找到targetNo的父节点
+            Node parent=searchParent(value);
+            //如果要删除的节点是叶子节点
+            if(targetNode.left==null&&targetNode.right==null){
+                 //判断targetNode是parent的左。右子节点
+                if(parent.left!=null&&parent.left.value==value){
+                    parent.left=null;
+                }else if(parent.right!=null&&parent.right.value==value){
+                    parent.right=null;
+                }
+            }else if(targetNode.left!=null&&targetNode.right!=null){//删除有两颗子树的节点
+                int minValue=delRightTreeMin(targetNode.right);
+                targetNode.value=minValue;
+                //也可以不用上面的思路，在左子树中找最大的
+            }else{//删除只有一颗子树的节点
+                //如果要删除的节点有左子节点
+                if(targetNode.left!=null){
+                    //如果targetNode是parent的左/右子节点
+                    if(parent.left.value==value){
+                        parent.left=targetNode.left;
+                    }else{
+                        parent.right=targetNode.left;
+                    }
+                }else{//要删除的节点有右子节点
+                   if(parent.left.value==value){
+                       parent.left=targetNode.right;
+                   }else{
+                       parent.right=targetNode.right;
+                   }
+                }
+            }
+
+        }
+
     }
 }
 
