48cc7fd12826da7572155fa60c30656d1322319e
ColorXJH <1204332316@qq.com>
2021-07-26 14:16:00 +0800

    update 14:15PM

--- src/main/java/com/master/chapter016/GraphTest.java
+++ src/main/java/com/master/chapter016/GraphTest.java
@@ -2,6 +2,8 @@ package com.master.chapter016;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * @author ColorXJH
@@ -13,6 +15,8 @@ public class GraphTest {
     private ArrayList<String> vertexList;//存储顶点的集合
     private int[][] edges;//存储图对应的邻接矩阵
     private int numberOfEdges;//表示边的数目
+    //定义数组boolean[],记录谋而节点是否被访问过
+    private boolean[] isVisited;
 
     public static void main(String[] args) {
         System.out.println("测试一把图是否创建ok");
@@ -33,6 +37,12 @@ public class GraphTest {
         test.insertEdge(1,4,1);
         //显示一把邻接矩阵
         test.showGraph();
+        //测试深度dfs
+        System.out.println("ss--------------------- -ss" );
+        //test.dfs();
+        //广度优先算法
+        System.out.println("gg-----------------------gg");
+        test.bfs();
 
     }
 
@@ -42,6 +52,111 @@ public class GraphTest {
         edges=new int[n][n];
         vertexList=new ArrayList<>(n);
         numberOfEdges=0;
+        isVisited=new boolean[n];
+    }
+
+    //得到第一个邻接节点的下标w
+    /**
+     * Description:
+     * @Author: ColorXJH
+     * @Date: 2021/7/26 10:53
+     * @param index
+     * @Return: int 如果存在就返回对应的下标，否则返回-1
+     **/
+    public int getFirstNeighbor(int index){
+        for(int j=0;j<vertexList.size();j++){
+            if(edges[index][j]>0){
+
+                return j;
+            }
+        }
+        return -1;
+    }
+
+    //根据前一个邻接节点的下标来获取下一个邻接节点
+    public int getNextNeighbor(int v1,int v2){
+        for(int j=v2+1;j<vertexList.size();j++){
+            if(edges[v1][j]>0){
+                return j;
+            }
+        }
+        return -1;
+    }
+
+
+    //深度优先遍历算法
+    public void dfs(boolean[] isVisited,int i){
+        //首先访问该节点：输出
+        System.out.print(getValueByIndex(i)+"->");
+        //将该节点设置为已经访问过
+        isVisited[i]=true;
+        //查找节点i的第一个邻接节点w
+        int w=getFirstNeighbor(i);
+        while(w!=-1){//说明有邻接节点
+               if(!isVisited[w]){//没有被访问过
+                    dfs(isVisited,w);
+               }
+               //如果w节点已经被访问过,查找邻接节点的下一个节点
+               w=getNextNeighbor(i,w);
+        }
+
+        //如果w不存在，则回到第一步，将从v的下一个节点继续:(见方法重载)，类似循环遍历所有的节点，然后进行dfs
+
+    }
+
+
+    //对dfs进行一个重载，遍历所有的节点，并进行dfs
+    public void dfs(){
+        //遍历所有的节点，进行dfs[回溯]
+        for(int i=0;i<getNumberOfVertex();i++){
+            if(!isVisited[i]){
+                dfs(isVisited,i);
+            }
+        }
+    }
+
+
+    //对一个节点进行广度优先遍历的方法
+    public void bfs(boolean[] isVisited,int i){
+        int u;//表示队列的头结点对应的下标
+        int w;//表示邻接节点w
+        //队列，记录节点访问的顺序
+        LinkedList queue=new LinkedList<>();
+        //访问节点
+        System.out.print(getValueByIndex(i)+"->");
+        //标记为已访问
+        isVisited[i]=true;
+        //将节点加入队列
+        queue.addLast(i);
+
+        while(!queue.isEmpty()){
+           u=(int) queue.removeFirst();//取出队列的头部
+             //得到第一个邻接节点的下标w
+            w=getFirstNeighbor(u);
+
+            while(w!=-1){//找到
+                //是否访问过
+                if(!isVisited[w]){
+                    System.out.print(getValueByIndex(w)+"->");
+                    //标记已经访问
+                    isVisited[w]=true;
+                    //入队列
+                    queue.addLast(w);
+                }
+                //以u这一行的，w后面的一个邻接节点（可以画出二维数组塦帮助理解）
+                w=getNextNeighbor(u,w);
+            }
+        }
+
+    }
+
+    //遍历所有的节点，都进行广度优先搜素算法
+    public void bfs(){
+        for(int i=0;i<getNumberOfVertex();i++){
+            if(!isVisited[i]){
+                bfs(isVisited,i);
+            }
+        }
     }
 
     //插入节点
@@ -129,4 +244,33 @@ public class GraphTest {
         //代码实现:见 main
 
 //图的深度优先遍历介绍
-    //图的遍历介绍：
+    //图的遍历介绍：所谓图的遍历，即是对节点的访问，一个图有那么多个节点，如何遍历这些节点，需要特定的策略，一般有两种访问策略：
+        //深度优先遍历   广度优先遍历
+    //1:深度优先遍历基本思想 depth first search  dfs
+        //1:深度优先遍历，从初始访问节点出发，初始访问节点可能有多个邻接节点，深度优先遍历的策略就是，首先访问第一个邻接节点，然后再以
+            //这个被访问的邻接节点作为初始节点，访问他的邻接节点，可以这样理解，每次都在访问万当前节点后首先访问当前节点的第一个邻接节点
+        //2:这样的访问策略是优先纵向挖掘深入，而不是对一个节点的所有邻接节点的横向遍历
+        //3:深度优先遍历是一个递归的过程
+            //算法步骤：
+                //1:访问初始节点v,并标记v节点已访问
+                //2:查找v节点的第一个邻接节点w
+                //3:若w存在，则继续执行4，如果w不存在，则回到第一步，将从v的下一个节点继续
+                //4:若w未被访问，对w进行深度优先遍历递归（即把w当做另一个v,然后进行步骤123）
+                //5:查找节点v的w节点的下一个邻接节点，转到步骤3
+
+//图的广度优先遍历基本思想 bfs
+    //图的广度优先搜索（Broad First Search）,类似于一个分层搜索的过程，广度优先遍历需要使用一个队列以保持访问过的节点的顺序，以便按这个
+        //顺序来访问这些节点的邻接节点
+    //算法步骤：
+        //1：访问初始节点v并标记节点v为已访问
+        //2: 节点v如队列
+        //3: 当队列非空时，继续执行，否则算法结束(对v节点而言，但是在整个节点而言，需要使用for循环介入)
+        //4: 出队列，取得队头节点u
+        //5: 查找节点u的第一个邻接节点w
+        //6: 若节点u的邻接节点w不存在，则转移到步骤3，否则循环执行以下三个步骤：
+            //1:若节点w尚未被访问，则访问节点w,并标记为已访问
+            //2:节点w入队列
+            //3:查找节点u的继w邻接节点后的下一个邻接节点w,转到步骤6
+
+
+//深度优先和广度优先的区别：顺序并不一样
---
+++ src/main/java/com/master/chapter017/ITUsuallyAlgorithm.java
@@ -0,0 +1,76 @@
+package com.master.chapter017;
+
+/**
+ * @Description: 程序员常用的十种算法
+ * @Author: ColorXJH
+ * @CreateDate: 2021/7/26 13:30
+ * @Version: 1.0
+ */
+public class ITUsuallyAlgorithm {
+    public static void main(String[] args) {
+        int[]array={1,3,8,10,11,67,100};
+        int index=BinarySearchNoRecursion.binarySearch(array,-8);
+        System.out.println(index);
+    }
+}
+
+//二分查找算法（非递归）
+    //前面有学习过二分查找递归算法，现在看非递归
+    //二分查找只适用于从有序的数列中进行查找（比如数字、字母），将数列排序后再进行查找
+    //二分查找的时间为对数时间O(log2N),即查找到需要的目标位置最多需要log2N步，假设0-99队列（100个数），寻找目标30 ，最多需要log2 100,即最多需要7次
+
+//代码实现：【1,3，8,10，11,67，100】，编程使用二分查找，非递归方式
+class BinarySearchNoRecursion{
+    /**
+     * Description: 二分查找非递归实现，array升序排列
+     * @Author: ColorXJH
+     * @Date: 2021/7/26 13:46
+     * @param array 带查找数组
+     * @param target  需要查找的数据
+     * @Return: int 返回对应下标，-1表示没找到
+     **/
+    public static int binarySearch(int[] array,int target){
+        int left=0;
+        int right=array.length-1;
+        while(left<=right){
+            int mid=(left+right)/2;
+            if(array[mid]==target){
+                return mid;
+            }else if(array[mid]>target){
+                right=mid-1;//向左边查找
+            }else{
+                left=mid+1;//向右边查找
+            }
+        }
+        return-1;
+    }
+}
+
+
+
+//分治算法
+    //分治算法是一种很重要的算法，字面理解为分而治之，就是把复杂的问题分成两个或更多的相同或相似的子问题，再把子问题分成更小的子问题，知道最后的子问题
+    //可以简单的求解，原问题的解即等于子问题的解的合并，这个技巧是很多高效算法的基础，如排序算法（快排，归并），傅里叶变换（快速傅里叶变换）...
+    //分治算法可以求解的经典问题：
+        //二分查找
+        //大整数乘法
+        //棋盘覆盖
+        //合并排序
+        //快速排序
+        //线性时间选择
+        //最接近点对问题
+        //循环赛日程表
+        //汉诺塔
+    //分治算法基本步骤
+        //1:分解：将原问题分解为若干个规模较小，相互独立，与原问题形式相同的子问题
+        //2:解决：若子问题规模较小而容易被解决则直接解决，否则递归的解决各个子问题
+        //3:合并：将各个子问题的解合并为原问题的解
+    //设计模式如下：
+        //if |p|<=n0 then return (ADHOC(P))
+        //将P问题分解为较小的子问题，P1，P2，P3...
+        //for i<-1 to k
+        //do yi<- Devide-and-Conquer(Pi)//递归解决Pi
+        //T<-merge(y1,y2,y3...yk)//合并子问题
+        //return T
+        //其中|p|表示问题p的规模，n0为一阈值，表示当问题p的规模不超过n0时，问题已经容易直接解除，不必要再继续分解，ADHOC(P)是该分治法中的基本子算法
+        //merge是分治法中的合并子算法 ，用于将问p的所有子问题p1,p2,p3...  pk的相应解页,y2,y3...yk合并为p的解
