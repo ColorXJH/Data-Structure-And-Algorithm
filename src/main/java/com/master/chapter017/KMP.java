f31eee14ebe07c7f9a923d2afae3d91e68b9b54c
ColorXJH <1204332316@qq.com>
2021-10-09 16:26:02 +0800

    reset

--- src/main/java/com/master/chapter017/KMP.java
+++ src/main/java/com/master/chapter017/KMP.java
@@ -1,6 +1,8 @@
 package com.master.chapter017;
 
 
+import java.util.Arrays;
+
 /**
  * @ClassName: KMP算法
  * @Package: com.master.chapter017
@@ -70,8 +72,38 @@ public class KMP {
         public static void main(String[] args) {
             String str1="BBC ABCDAB ABCDABCDABDE";
             String str2="ABCDABD";
-            int[]next=kmpNext("AA");//[0,1]
-            System.out.println();
+            int[]next=kmpNext("ABCDABD");//[0, 0, 0, 0, 1, 2, 0]
+            System.out.println("next="+ Arrays.toString(next));
+            int index=KmpSearch(str1,str2,next);
+            System.out.println("index="+index);
+        }
+
+        //写出我们的kmp搜索算法
+        /**
+         * 功能描述:
+         * @param: str1 源字符串
+         * @param: str2 子串
+         * @param: next 部分匹配表，是子串对应的部分匹配表
+         * @Return: int  -1 没有匹配到 否则返回第一个匹配的位置
+         * @Author: ColorXJH
+         * @Date: 2021/10/9 15:43
+         */
+        public static int KmpSearch(String str1,String str2,int[]next){
+            //遍历
+            for(int i=0,j=0;i<str1.length();i++){
+                //需要处理str1.charAt(i)!=str2.charAt(j)
+                //Kmp算法的核心点
+                while(j>0&&str1.charAt(i)!=str2.charAt(j)){
+                    j=next[j-1];
+                }
+                if(str1.charAt(i)==str2.charAt(j)){
+                    j++;
+                }
+                if(j==str2.length()){//找到了
+                    return i-j+1;
+                }
+            }
+            return -1;
         }
 
         //获取到一个字符串（子串）的部分匹配值表
@@ -80,6 +112,11 @@ public class KMP {
             int[] next=new int[dest.length()];
             next[0]=0;//如果字符串长度为1，部分匹配值就是0
             for(int i=1,j=0; i<dest.length();i++){
+                //当dest.charAt(i)!=dest.charAt(j),我们需要从next[j-1]获取新的j,直到我们发现有条件满足est.charAt(i)==dest.charAt(j)
+                //这是kmp算法的核心点
+                while(j>0&&dest.charAt(i)!=dest.charAt(j)){
+                    j=next[j-1];
+                }
                 //当dest.charAt(i)==dest.charAt(j)条件满足时，部分匹配值就+1
                 if(dest.charAt(i)==dest.charAt(j)){j++;}
                 next[i]=j;
