22fa615359e923b46266b88ae6c28ef810c8fb8c
ColorXJH <1204332316@qq.com>
2021-10-17 13:43:33 +0800

    update 14:15PM

--- src/main/java/com/master/chapter017/Prim.java
+++ src/main/java/com/master/chapter017/Prim.java
@@ -1,5 +1,8 @@
 package com.master.chapter017;
 
+import javax.swing.text.TableView;
+import java.util.Arrays;
+
 /**
  * @ClassName: Prim  普利姆算法
  * @Package: com.master.chapter017
@@ -8,6 +11,125 @@ package com.master.chapter017;
  * @author: ColorXJH
  */
 public class Prim {
+    public static void main(String[] args) {
+        //测试图是否创建成果
+        char[]data=new char[]{'A','B','C','D','E','F','G'};
+        int verxs=data.length;
+        //邻接矩阵的关系使用二维数组描述,10000表示2个点不连通，其他表示距离
+        int[][]weight=new int[][]{
+                {10000,5,7,10000,10000,10000,2},
+                {5,10000,10000,9,10000,10000,3},
+                {7,10000,10000,10000,8,10000,10000},
+                {10000,9,10000,10000,10000,4,10000},
+                {10000,10000,8,10000,10000,5,4},
+                {10000,10000,10000,4,5,10000,6},
+                {2,3,10000,10000,4,6,10000}
+        };
+        //创建MGraph对象
+        MGraph mGraph=new MGraph(verxs);
+        //创建一个MinTree对象
+        MinTree tree=new MinTree();
+        tree.createGraph(mGraph,verxs,data,weight);
+        //输出
+        tree.showGraph(mGraph);
+        //测试普利姆算法
+        tree.prim(mGraph,0);
+
+    }
+
+}
+
+//创建最小生成树-》村庄的图
+class MinTree{
+    //创建图的邻接矩阵
+    /**
+     * Description: 
+     * @Author: ColorXJH
+     * @Date: 2021/10/17 12:37
+     * @param graph 图对象
+     * @param verxs 图对应的顶点个数
+     * @param data 图的各个顶点的值
+     * @param weight 图的邻接矩阵
+     * @Return: void
+     **/
+    public void createGraph(MGraph graph,int verxs, char[]data,int[][]weight){
+        for (int i = 0; i <verxs ; i++) {//顶点
+            graph.data[i]=data[i];
+            for (int j= 0;  j<verxs ; j++) {
+                graph.weight[i][j]=weight[i][j];
+            }
+            
+        }
+    }
+    //显示图的邻接矩阵
+    public void showGraph(MGraph graph){
+        for (int[] link: graph.weight){
+            System.out.println(Arrays.toString(link));
+        }
+    }
+
+    //编写一个prim普利姆算法，得到最小生成树
+    /**
+     * Description: 
+     * @Author: ColorXJH
+     * @Date: 2021/10/17 12:57
+     * @param graph 图
+     * @param v 从图的第几个顶点开始生成，‘A’-->0，'B'-->1....
+     * @Return: void
+     **/
+    public  void prim(MGraph graph,int v){
+        //标记结点（顶点）是否被访问过
+        int[]visited= new int[graph.verxs];
+        //visited[]默认的元素的值都为0，表示没有访问过
+        for (int i = 0; i < graph.verxs ; i++) {
+            visited[i]=0;
+        }
+        //把当前这个结点标记为已访问
+        visited[v]=1;
+        //h1,h2记录两个顶点的下标
+        int h1=-1;
+        int h2=-1;
+        int totalWieght=0;
+        int minWeight=10000;//将minWeight初始化为一个较大的值，，后面在遍历的过程中，会被替换
+        for (int k = 1; k < graph.verxs; k++) {//因为有graph.verxs个顶点，普利姆算法结束后，有graph.verxs-1条边
+            //确定每一次生成的子图和哪个节点和这次遍历的这个节点的距离最近
+            for (int i = 0; i < graph.verxs ; i++) {//遍历已经访问过的节点，i节点表示被访问过的节点
+                for (int j = 0; j < graph.verxs ; j++) {//遍历所有没有访问过的节点，j节点表示还没有访问过的节点
+                    //visited[i]==1表示被访问过，visited[j]==0表示没有被访问过，graph.weight[i][j]表示距离
+                    if(visited[i]==1&&visited[j]==0&&graph.weight[i][j]<minWeight){
+                        //替换minweight，寻找已经访问过的额节点和未访问过的节点间的全脂最小的边
+                        minWeight=graph.weight[i][j];
+                        //同时记录下节点的位置
+                        h1=i;
+                        h2=j;
+
+                    }
+                }
+            }
+            //找到一条边是最小的边
+            System.out.println("边<"+graph.data[h1]+","+graph.data[h2]+"> 权值是："+minWeight);
+            //将当前找到的节点标记为已经访问
+            visited[h2]=1;
+            totalWieght+=minWeight;
+            //minweight 重置
+            minWeight=10000;
+        }
+        System.out.println("总权值为："+totalWieght);
+
+    }
+}
+
+
+class MGraph{
+    int verxs;//表示图的节点个数
+    char[] data;//存放节点的数据
+    int[][]weight;//存放边，这就是我们的邻接矩阵
+
+    public MGraph(int verxs){
+        this.verxs=verxs;
+        data=new char[verxs];
+        weight=new int[verxs][verxs];
+    }
 }
 
 //应用场景--修路问题
@@ -37,3 +159,4 @@ public class Prim {
                 //标记visited[vj]=1
             //4重复步骤2，知道U与V相等，即所有顶点都被标记为访问过，此时D中有n-1条边
             //5提示：单独看步骤很难理解：，通过代码比较好理解-169
+//next 172
